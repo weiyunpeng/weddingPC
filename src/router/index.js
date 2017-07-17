8bcc5cff8898696ce55070cc73f98db70225821f
李煌 <dukeli@qqdyw.net>
2017-07-17 11:00:47 +0800

    jshint 修改

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,19 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+/*jshint esversion:6*/
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 
 //状态控制
-import store from '../store/index'
+import store from '../store/index';
 
 //页面控制
-import index from '../page/index'
-import meal from '../page/meal.vue'
-import busDeatils from '../page/busDetails.vue'
+import index from '../page/index';
+import meal from '../page/meal.vue';
+import busDeatils from '../page/busDetails.vue';
 
 //组件控制
-import mealList from '../components/meal/mealList.vue'
+import mealList from '../components/meal/mealList.vue';
 
 const routes = [{
     path: '/',
