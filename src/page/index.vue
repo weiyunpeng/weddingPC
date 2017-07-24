ff66a814dcc369c69ee226ba18476329c2244737
yun <153967808@qq.com>
2017-07-24 19:19:33 +0800

    商家和套餐列表进行状态管理

--- src/page/index.vue
+++ src/page/index.vue
@@ -8,7 +8,7 @@
         <ul class="list">
             <li class="list_con" v-for="item in busList" :key="item.business.id">
                 <div class="shop">
-                    <router-link :to="{ name: 'busDeatils', params: {name:item.business.name}}" target="_blank">
+                    <router-link :to="{ name: 'busDeatils', params: {}}" target="_blank">
                         <img class="shop_logo" v-lazy="item.business.logo">
                         <ul class="shop_details">
                             <li>
