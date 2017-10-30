59f86dc1cafd9f13ef9820fb7d693ae862aef04a
yun <153967808@qq.com>
2017-10-30 11:33:30 +0800

    解决nginx使用history模式加载

--- server/server/router.js
+++ server/server/router.js
@@ -2,13 +2,13 @@
 // 路由注册
 function router_reg(app){
     //
-    app.all('/store/:name', require('./index/index').index);
-    app.all('/package/:name', require('./index/package').index);
-    app.all('/cameraman', require('./man/cameraman').index);
-    app.all('/makeup', require('./man/makeup').index);
-    app.all('/site/:name', require('./site/index').index);
-    app.all('/user/:name', require('./user/index').index);
-    app.all('/guide/:name', require('./guide/index').index);
+    app.all('/api/store/:name', require('./index/index').index);
+    app.all('/api/package/:name', require('./index/package').index);
+    app.all('/api/cameraman', require('./man/cameraman').index);
+    app.all('/api/makeup', require('./man/makeup').index);
+    app.all('/api/site/:name', require('./site/index').index);
+    app.all('/api/user/:name', require('./user/index').index);
+    app.all('/api/guide/:name', require('./guide/index').index);
 }
 
 
